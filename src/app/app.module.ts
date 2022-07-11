82ceb11d6937f40ef1b10b3464a2ca3b7f7727a7
elazar-gadasi <106330969+elazar-gadasi@users.noreply.github.com>
2022-07-11 19:08:32 +0300

    binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routes/app-routing.module';
+import { AppComponent } from './root/app.component';
+
+import { BindingComponent } from './component/binding/binding.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BindingComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component/binding/binding.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css'],
+})
+export class BindingComponent {
+  isHidden: boolean = false;
+
+  image: img = {
+    url: 'https://cdn.pixabay.com/photo/2022/06/27/18/55/grain-7288138__340.jpg',
+    alt: 'תבואה',
+  };
+
+  toggle(): boolean {
+    return (this.isHidden = this.isHidden ? false : true);
+  }
+}
+interface img {
+  url: string;
+  alt: string;
+}
